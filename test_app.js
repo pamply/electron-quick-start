352220fcfbb49f9f01d94012e0c0652d99cf06b5
Manuel Pamplona <Manuel.Pamplona.Rosado@gmail.com>
2017-11-07 10:24:46 -0600

    Adding snippet example

---
+++ test_app.js
@@ -0,0 +1,38 @@
+//A simple test to verify a visible window is opened with a title
+var Application = require('spectron').Application
+var assert = require('assert')
+
+var app = new Application({
+  path: '/home/vagrant/electron-quick-start/MyApp-linux-x64/MyApp'})
+
+console.log('Running app...')
+
+app.start()
+    .then(() => app.browserWindow.isVisible())
+    .then((isVisible) => console.log('Main window is visible: ', isVisible))
+    .then(() => {
+        console.log('Checking text...')
+        return app.client.getText('#par')    
+    })
+    .then((text) => {
+        assert.equal(text, "Does this work?")
+        console.log("Text: ", text)   
+    })
+    .then(() => {
+        console.log('Checking the windows title...')
+        return app.client.getTitle()
+    })
+    .then((title) => {
+        assert.equal(title, 'Hello World!')
+        console.log("Title: ", title)
+    })
+    .then(() => {
+        console.log('Stopping the application')
+        return app.stop()
+    })
+    .catch((error) => {
+        //Log any failures
+        console.error('Test failed: ', error.message)
+        console.log('Stopping the application')
+        return app.stop()
+    })
